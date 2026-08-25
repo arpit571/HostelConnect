e62bf0f83814bc9d22f8081eda911a99308232e3
Aarush Rai <aarush0054@gmail.com>
2026-08-25 23:05:44 +0530

    Resolve server merge conflict

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ console.log(
 
 const express = require("express");
 const connectDB = require("./config/db");
+
 const complaintRoutes = require("./routes/complaintRoutes");
+const complaintEvidenceRoutes = require("./routes/complaintEvidenceRoutes");
+
 const userRoutes = require("./routes/userRoutes");
 const noticeRoutes = require("./routes/noticeRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
@@ -15,28 +18,25 @@ const aiComplaintRoutes = require("./routes/aiComplaintRoutes");
 
 const cors = require("cors");
 
-
-
 const app = express();
-
 
 app.use(cors());
 
 connectDB();
 
 app.use(express.json());
+
 app.use("/notices", noticeRoutes);
 app.use("/auth", userRoutes);
 app.use("/dashboard", dashboardRoutes);
 
-
 app.use("/complaints", complaintRoutes);
+app.use("/complaints", complaintEvidenceRoutes);
+
 app.use("/api/ai", aiComplaintRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-    console.log(`Server running on port ${PORT}`);
-
+  console.log(`Server running on port ${PORT}`);
 });
